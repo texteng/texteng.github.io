6a82a55110c2ba96bc327f220c4c2854b7958100
Stephen Teng <3964899+texteng@users.noreply.github.com>
2018-09-09 17:03:08 -0500

    added more to front page

---
+++ Javascript/main.js
@@ -0,0 +1,13 @@
+$(document).on("mouseenter", "#ptimage", function() {
+    this.src = "images/PeriodicTableactive.png"
+  });
+$(document).on("mouseleave ", "#ptimage", function() {
+  this.src = "images/PeriodicTable.png"
+});
+
+$(document).on("mouseenter", "#pacmanimage", function() {
+  this.src = "images/PacMan-active.gif"
+});
+$(document).on("mouseleave ", "#pacmanimage", function() {
+  this.src = "images/PacMan.gif"
+});
