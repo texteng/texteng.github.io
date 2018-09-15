2b22a6f89062f8e8c0b55d5513e51e9b8ae4f2e5
Stephen Teng <3964899+texteng@users.noreply.github.com>
2018-09-15 09:43:15 -0500

    converted css to scss. removed stable version into non git folder.

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,13 +1,13 @@
 $(document).on("mouseenter", "#ptimage", function() {
-    this.src = "images/PeriodicTableactive.png"
-  });
+  this.src = "images/PeriodicTableactive.png";
+});
 $(document).on("mouseleave ", "#ptimage", function() {
-  this.src = "images/PeriodicTable.png"
+  this.src = "images/PeriodicTable.png";
 });
 
 $(document).on("mouseenter", "#pacmanimage", function() {
-  this.src = "images/PacMan-active.gif"
+  this.src = "images/PacMan-active.gif";
 });
 $(document).on("mouseleave ", "#pacmanimage", function() {
-  this.src = "images/PacMan.gif"
+  this.src = "images/PacMan.gif";
 });
