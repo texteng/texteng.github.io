e35e676d99f5e3004ee133f8219d8323882b9951
Stephen Teng <3964899+texteng@users.noreply.github.com>
2018-09-24 19:34:40 -0500

    Major! Added sass for custombootstrap buttons.  Custom color schemes

--- PeriodicTable/javascript/Colorlibraries.js
+++ PeriodicTable/javascript/Colorlibraries.js
@@ -49,51 +49,51 @@ const bondtypeDict = {
 const yearDiscoveredDict = {
     "title":"Year Discovered By",
     "Ancient": "light",
-    1800:"skyblue",
-    1850: "info",
-    1900:"darkgreen", 
-    1950:"success", 
-    2000:"warning",
-    2018: "danger"
+    1800:"yeardiscovered-1",
+    1850:"yeardiscovered-2",
+    1900:"yeardiscovered-3", 
+    1950:"yeardiscovered-4", 
+    2000:"yeardiscovered-5",
+    2018:"yeardiscovered-6"
 }
 
 const densityDict = {
     "title":"Density g/L",
-    1:"danger", 
-    2:"warning", 
-    3:"success", 
-    4:"skyblue", 
-    5:"info", 
-    10:"primary", 
-    20:"darkgreen",
-    30:"purple",
-    50:"dark",
+    1:"densitycolors-1", 
+    2:"densitycolors-2", 
+    3:"densitycolors-3", 
+    4:"densitycolors-4", 
+    5:"densitycolors-5", 
+    10:"densitycolors-6", 
+    20:"densitycolors-7",
+    30:"densitycolors-8",
+    50:"densitycolors-9",
 }
 
 const meltDict = {
     "title":"Melting/Freezing Point (K) (Note 273 is 0 C)",
-    50: "dark",
-    100:"purple",
-    250:"skyblue", 
-    273:"info", 
-    374:"primary", 
-    500:"darkgreen",
-    1000:"success",
-    2000:"warning",
-    5000: "danger"
+    50: "coldcolors-1",
+    100:"coldcolors-2",
+    250:"coldcolors-3", 
+    273:"coldcolors-4", 
+    374:"coldcolors-5", 
+    500:"coldcolors-6",
+    1000:"coldcolors-7",
+    2000:"coldcolors-8",
+    5000: "coldcolors-9"
 }
 
 const boilDict = {
     "title":"Boiling Point (K) (Note 374 is 100 C)",
-    50: "dark",
-    100:"purple",
-    250:"skyblue", 
-    273:"info", 
-    374:"primary", 
-    1000:"darkgreen",
-    3000:"success",
-    5000:"warning",
-    7000: "danger"
+    50: "heatcolors-1",
+    100:"heatcolors-2",
+    250:"heatcolors-3", 
+    273:"heatcolors-4", 
+    374:"heatcolors-5", 
+    1000:"heatcolors-6",
+    3000:"heatcolors-7",
+    5000:"heatcolors-8",
+    7000: "heatcolors-9"
 }
 const electronegativityDict = {
     "title":"Electronegativity",
@@ -106,25 +106,25 @@ const electronegativityDict = {
 const atomicRadiusDict = {
     "title":"Atomic Radius (Angstroms &#197;) ",
     "Unknown": "light",
-    50: "primary",
-    100: "skyblue",
-    125: "info",
-    150: "darkgreen",
-    175: "success",
-    200: "warning",
-    250: "danger",
+    50: "atomicRadius-1",
+    100: "atomicRadius-2",
+    125: "atomicRadius-3",
+    150: "atomicRadius-4",
+    175: "atomicRadius-5",
+    200: "atomicRadius-6",
+    250: "atomicRadius-7",
 }
 
 const ionizationEnergyDict = {
     "title":"Ionization Energy (Electron Volts eV)",
     "Unknown": "light",
-    500: "primary",
-    750: "skyblue",
-    1000: "info",
-    1250: "darkgreen",
-    1500: "success",
-    2000: "warning",
-    2500: "danger",
+    500: "ionizationEnergy-1",
+    750: "ionizationEnergy-2",
+    1000: "ionizationEnergy-3",
+    1250: "ionizationEnergy-4",
+    1500: "ionizationEnergy-5",
+    2000: "ionizationEnergy-6",
+    2500: "ionizationEnergy-7",
 }
 // const electronAffinityDict= {
 //     "None": "light",
@@ -148,3 +148,4 @@ function colorLibrary(){
     }
     return library[currentcategory];
 }
+
